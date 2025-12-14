d2d6e568fd63096b9cbdf2f15de415c88015ab64
Sultan <138652076+sulfar0@users.noreply.github.com>
2025-12-14 20:06:55 +0700

    update

--- docs/sbctl/sbctl-sign.sh
+++ docs/sbctl/sbctl-sign.sh
@@ -1,27 +1,19 @@
 path=$(pwd)
 linux=($(ls /boot/efi/linux/))
 kernel=($(ls /boot/kernel | grep vmlinuz))
+
 function gen_secboot {
         sbctl create-keys &&
-        
+        sbctl sign --save /boot/efi/EFI/Boot/bootx64.efi &&
+        sbctl sign --save /boot/efi/EFI/systemd/systemd-bootx64.efi &&
         for efi in "${linux[@]}"; do
-                sbctl sign --save /boot/efi/linux/$efi
-        done &&
-        
-        sbctl sign --save /boot/EFI/Boot/bootx64.efi &&
-        sbctl sign --save /boot/EFI/systemd/systemd-bootx64.efi &&
-        
+                sbctl sign --save /boot/efi/EFI/linux/$efi
+        done
         for vmlinuz in "${kernel[@]}"; do
                 sbctl sign --save /boot/kernel/$vmlinuz
-        done &&
+        done
         
-        cp $path/sbctl-enroll-script.sh /usr/local/bin/ &&
-        cp $path/sbctl-sign.service /etc/systemd/system/ &&
-        sudo systemctl enable sbctl-sign &&
-
         sbctl enroll-keys -m -i &&
         sbctl verify | sed -E 's|^.* (/.+) is not signed$|sbctl sign -s "\1"|e'
-        touch /var/lib/sbctl-enroll-script
-
 }
 gen_secboot
